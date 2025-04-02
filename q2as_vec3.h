c33dbc2f005b790b0b913451895ed6b5a485b62d
Oskar Mendel <brokenprogrammer@gmail.com>
2025-04-02 23:14:36 +0200

    Replacement vec3 type (#13)
    * WIP on adding non gpl version of vector3 type.
    
    * Slotted new vector type into angelscript.
    
    * Updated comparison and remove zero initialization.
    
    * Fixed error in normalize.

---
+++ q2as_vec3.h
@@ -0,0 +1,247 @@
+struct vec3
+{
+	union
+	{
+		struct 
+		{
+			float x;
+			float y;
+			float z;
+		};
+
+		std::array<float, 3> Elements;
+	};
+
+	float &operator[] (unsigned int i) {
+		i = i > 2 ? 0 : i;
+
+		return Elements[i];
+	}
+
+	const float &operator[] (unsigned int i) const {
+		i = i > 2 ? 0 : i;
+
+		return Elements[i];
+	}
+
+	bool operator==(const vec3 &v) const {
+		return Elements == v.Elements;
+	}
+
+	bool equals(const vec3 &v, const float &epsilon) const {
+		auto rx = abs(x - v.x) <= epsilon * max(1.0f, max(abs(x), abs(v.x)));
+		auto ry = abs(y - v.y) <= epsilon * max(1.0f, max(abs(y), abs(v.y)));
+		auto rz = abs(z - v.z) <= epsilon * max(1.0f, max(abs(z), abs(v.z)));
+
+		return rx && ry && rz;
+	}
+
+	explicit operator bool() const {
+		return x || y || z;
+	}
+
+	float dot(const vec3 &v) const {
+		return x * v.x + y * v.y + z * v.z;
+	}
+
+	vec3 scaled(const vec3 &v) const {
+		vec3 result;
+
+		result.x = x * v.x;
+		result.y = y * v.y;
+		result.z = z * v.z;
+
+		return result;
+	}
+
+	vec3 &scale(const vec3 &v) {
+		x = x * v.x;
+		y = y * v.y;
+		z = z * v.z;
+
+		return *this;
+	}
+
+	vec3 operator-(const vec3 &v) const {
+		vec3 result;
+
+		result.x = x - v.x;
+		result.y = y - v.y;
+		result.z = z - v.z;
+
+		return result;
+	}
+
+	vec3 operator+(const vec3 &v) const {
+		vec3 result;
+
+		result.x = x + v.x;
+		result.y = y + v.y;
+		result.z = z + v.z;
+
+		return result;
+	}
+
+	vec3 operator/(const vec3 &v) const {
+		vec3 result;
+
+		result.x = x / v.x;
+		result.y = y / v.y;
+		result.z = z / v.z;
+
+		return result;
+	}
+
+	vec3 operator/(const float &v) const {
+		vec3 result;
+
+		result.x = x / v;
+		result.y = y / v;
+		result.z = z / v;
+
+		return result;
+	}
+
+	vec3 operator/(const int &v) const {
+		vec3 result;
+
+		result.x = x / v;
+		result.y = y / v;
+		result.z = z / v;
+
+		return result;
+	}
+
+	vec3 operator*(const float &v) const {
+		vec3 result;
+
+		result.x = x * v;
+		result.y = y * v;
+		result.z = z * v;
+
+		return result;
+	}
+
+	vec3 operator*(const int &v) const {
+		vec3 result;
+
+		result.x = x * v;
+		result.y = y * v;
+		result.z = z * v;
+
+		return result;
+	}
+
+	vec3 operator-() const {
+		vec3 result;
+
+		result.x = -x;
+		result.y = -y;
+		result.z = -z;
+
+		return result;
+	}
+
+	vec3 &operator-=(const vec3 &v) {
+		x -= v.x;
+		y -= v.y;
+		z -= v.z;
+
+		return *this;
+	}
+
+	vec3 &operator+=(const vec3 &v) {
+		x += v.x;
+		y += v.y;
+		z += v.z;
+
+		return *this;
+	}
+
+	vec3 &operator/=(const vec3 &v) {
+		x /= v.x;
+		y /= v.y;
+		z /= v.z;
+
+		return *this;
+	}
+
+	vec3 &operator/=(const float &v) {
+		x /= v;
+		y /= v;
+		z /= v;
+
+		return *this;
+	}
+
+	vec3 &operator/=(const int &v) {
+		x /= v;
+		y /= v;
+		z /= v;
+
+		return *this;
+	}
+
+	vec3 &operator*=(const float &v) {
+		x *= v;
+		y *= v;
+		z *= v;
+
+		return *this;
+	}
+
+	vec3 &operator*=(const int &v) {
+		x *= v;
+		y *= v;
+		z *= v;
+
+		return *this;
+	}
+
+	float length() const {
+		return sqrtf(lengthSquared());
+	}
+
+	float lengthSquared() const {
+		return (x * x + y * y + z * z);
+	}
+
+	vec3 normalized() const {
+		vec3 result = {x, y, z};
+		result.normalize();
+
+		return result;
+	}
+
+	vec3 normalized(float &v) const {
+		vec3 result = { x, y, z };
+		v = result.normalize();
+
+		return result;
+	}
+
+	float normalize() {
+		float len = length();
+		
+		float f = len;
+		if (len > 0.0f) {
+			f = 1.0f / len;
+		}
+
+		x *= f;
+		y *= f;
+		z *= f;
+
+		return len;
+	}
+
+	vec3 cross(const vec3 &v) const {
+		vec3 result;
+		
+		result.x = y * v.z - z * v.y;
+		result.y = z * v.x - x * v.z;
+		result.z = x * v.y - y * v.x;
+		
+		return result;
+	}
+};
