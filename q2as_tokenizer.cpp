7a77e884103c53027153cc769bf6db5456967622
Oskar Mendel <brokenprogrammer@gmail.com>
2025-04-17 03:34:59 +0200

    Non-GPL implementation of ParseView. (#18)
    * non gpl ParseView.
    
    * Replace COM_ParseView.

--- q2as_tokenizer.cpp
+++ q2as_tokenizer.cpp
@@ -81,7 +81,7 @@ struct tokenizer_t : q2as_ref_t
     // returns true if not EOF.
     inline bool next()
     {
-        cur().token = COM_ParseView(cur().view, separators.data());
+        cur().token = q2as_ParseView(cur().view, separators.data());
         return has_token();
     }
 
